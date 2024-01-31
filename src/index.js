215b4747b2ae949cc1c3c4aad11af020a5901cc4
drkchu <derekdbchu@gmail.com>
2024-01-31 11:28:11 -0800

    Add the bare bones of using weatherAPI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 // Importing styles
 import './styles/styles.css'
 
+// This function takes in an API call and returns the JSON object
+// Doesn't account for checks since I'm just doing this to practice :D
+async function weatherProcessorJSON(url) {
+    try {
+        const response = await fetch(url, {mode: 'cors'});
+        const data = await response.json();
+        return data
+    } catch (error) {
+        // Handling errors
+        alert("Something went wrong!")
+    }
+}
+
+const searchButton = document.querySelector('.weather-search')
+const countryDiv = document.querySelector('.country')
+const regionDiv = document.querySelector('.region')
+const temperatureDiv = document.querySelector('.temperature')
+const APILink = 'https://api.weatherapi.com/v1/current.json?key=a37dd5f12bbe456691d213231241601&q='
+searchButton.addEventListener('click', () => {
+    const currentCity = document.querySelector('.weather-input').value;
+    let country, region, temperature;
+
+    weatherProcessorJSON(APILink + currentCity).then((json) => {
+        console.log(json)
+
+        country = json.location.country
+        region = json.location.region
+        temperature = json.current.temp_c
+
+        countryDiv.textContent = `COUNTRY: ${country}`
+        regionDiv.textContent = `REGION: ${region}`
+        temperatureDiv.textContent = `TEMP IN CELSIUS: ${temperature}`
+    })
+});
